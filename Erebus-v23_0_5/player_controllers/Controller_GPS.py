19a032c27055fdca7c0b558c7f4d8d609a759291
Gerard <uribururomerogerardocdm@gmail.com>
2023-06-30 16:22:54 -0300

    Repositorio con el simulador

---
+++ Erebus-v23_0_5/player_controllers/Controller_GPS.py
@@ -0,0 +1,21 @@
+from controller import Robot, GPS
+
+timeStep = 32 
+
+robot = Robot()
+
+gps = robot.getDevice("gps")
+gps.enable(timeStep)
+
+wheel1 = robot.getDevice("wheel1 motor")
+wheel2 = robot.getDevice("wheel2 motor")
+
+start = robot.getTime()
+
+while robot.step(timeStep) != -1:
+
+    x = round(gps.getValues()[0]/tilesize)
+    y = round(gps.getValues()[1]/tilesize)
+    z = round(gps.getValues()[2]/tilesize)
+
+    print(f'x: {x}, y: {y}, z: {z}')
